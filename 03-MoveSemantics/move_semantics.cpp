c6f683f9b32d77ca9bdbb50a8339e87210940cf9
Tim Gleue <tim@gleue-interactive.com>
2024-04-07 18:37:05 +0200

    Improve program output

--- 03-MoveSemantics/move_semantics.cpp
+++ 03-MoveSemantics/move_semantics.cpp
@@ -9,58 +9,52 @@ int main() {
 
     // Copied/moved Messages
 
-    std::cout << "\n= Empty message:\n";
+    std::cout << "\nDefault construction:\n";
     Message<char, 0> emptyMsg;
-    std::cout << emptyMsg << std::endl;
+    std::cout << "\n=> Empty message: " << emptyMsg << std::endl;
 
-    std::cout << "\n= Original message:\n";
+    std::cout << "\nCustom construction:\n";
     std::array<int, 10> msgBody{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     Message<int, 10> originalMessage("tim", msgBody);
-    std::cout << originalMessage << std::endl;
+    std::cout << "\n=> Original message: " << originalMessage << std::endl;
 
-    std::cout << "\n= Copied message:\n";
+    std::cout << "\nCopy construction:\n";
     auto copiedMessage{originalMessage};
-    std::cout << copiedMessage << std::endl;
-    std::cout << "- Original message:\n";
-    std::cout << originalMessage << std::endl;
+    std::cout << "\n=> Copied message: " << copiedMessage << std::endl;
+    std::cout << "-> Original message: " << originalMessage << std::endl;
 
-    std::cout << "\n= Copy assigned message:\n";
+    std::cout << "\nDefault construction:\n";
     decltype(originalMessage) copyAssignedMessage;
+    std::cout << "\nCopy assigment:\n";
     copyAssignedMessage = originalMessage;
-    std::cout << copyAssignedMessage << std::endl;
-    std::cout << "- Original message:\n";
-    std::cout << originalMessage << std::endl;
+    std::cout << "\n=> Copy assigned message: " << copyAssignedMessage << std::endl;
+    std::cout << "-> Original message: " << originalMessage << std::endl;
 
-    std::cout << "\n= Moved message:\n";
+    std::cout << "\nMove construction:\n";
     auto movedMessage{std::move(originalMessage)};
-    std::cout << movedMessage << std::endl;
-    std::cout << "- Original message:\n";
-    std::cout << originalMessage << std::endl;
+    std::cout << "\n=> Moved message: " << movedMessage << std::endl;
+    std::cout << "-> Original message:" << originalMessage << std::endl;
 
-    std::cout << "\n= Move from message:\n";
+    std::cout << "\nRe-initialization of moved from object:\n";
     std::array<int, 10> altBody{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     originalMessage.resetID();
     originalMessage.setRecipient(std::move("foo"));
     originalMessage.setBody(std::move(altBody));
-    std::cout << originalMessage << std::endl;
+    std::cout << "=> Moved from message: " << originalMessage << std::endl;
 
     // Message Queues
 
     std::deque<Message<int, 10>> messageQueue{};
 
-    std::cout << "\n= Enqueue messages:\n";
+    std::cout << "\nEnqueing messages:\n";
     messageQueue.emplace_back(std::move(movedMessage));
-    std::cout << "- Original moved message:\n";
-    std::cout << movedMessage << std::endl;
-    messageQueue.emplace_back(std::move(originalMessage));
-    std::cout << "- Original message:\n";
-    std::cout << originalMessage << std::endl;
+    std::cout << "\n=> Enqueued message (moved): " << movedMessage << std::endl << std::endl;
+    messageQueue.emplace_back(originalMessage);
+    std::cout << "\n=> Enqueued message (copied): " << originalMessage << std::endl;
 
-    std::cout << "\n= Dequeue messages:\n";
-    const auto& firstMessage = messageQueue.front(); messageQueue.pop_front();
-    std::cout << "- First message:\n";
-    std::cout << firstMessage << std::endl;
-    const auto& nextMessage = messageQueue.front(); messageQueue.pop_front();
-    std::cout << "- Next message:\n";
-    std::cout << nextMessage << std::endl;
+    std::cout << "\nDequeing messages:\n";
+    auto firstMessage = messageQueue.front(); messageQueue.pop_front();
+    std::cout << "\n=> Dequeued first message: " << firstMessage << std::endl << std::endl;
+    auto nextMessage = messageQueue.front(); messageQueue.pop_front();
+    std::cout << "\n-> Dequeued next message: " << nextMessage << std::endl;
 }
