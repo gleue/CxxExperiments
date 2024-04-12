29e67b4fc35f6e516767a2d9840ad1c1202fb225
Tim Gleue <tim@gleue-interactive.com>
2024-04-12 14:33:27 +0200

    Add expriments returning values

--- 03-MoveSemantics/move_semantics.cpp
+++ 03-MoveSemantics/move_semantics.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <optional>
 #include <source_location>
 #include <utility>
 
@@ -8,7 +9,7 @@ void print(const std::source_location &location = std::source_location::current(
 
 struct Lifetime {
     Lifetime() noexcept { print(); }
-    Lifetime(int) noexcept { print(); }
+    explicit Lifetime(int) noexcept { print(); }
     Lifetime(const Lifetime &) noexcept { print(); }
     Lifetime(Lifetime &&) noexcept { print(); }
     
@@ -30,20 +31,47 @@ void callByRValueRef(Lifetime &&lt) {
     auto lifetime = std::move(lt);
 }
 
+// See: C++ Weekly - Ep 421 - You're Using optional, variant, pair, tuple, any, and expected Wrong!
+//      https://www.youtube.com/watch?v=0yJk5yfdih0
+
+Lifetime returnValue() {
+    return Lifetime{1985};
+}
+
+std::optional<Lifetime> returnOptional() {
+    return std::optional<Lifetime>{1985};
+}
+
+std::optional<Lifetime> returnOptionalValue() {
+    return Lifetime{1985};
+}
+
 int main() {
-    auto lt = Lifetime(1);
+    {
+        auto lt = Lifetime{1};
 
-    std::puts("\nValue:");
-    callByValue(lt);
+        std::puts("\nPass by value:");
+        callByValue(lt);
 
-    std::puts("\nConst Ref:");
-    callByConstRef(lt);
+        std::puts("\nPass by const ref:");
+        callByConstRef(lt);
 
-    std::puts("\nRvalue Ref:");
-    callByRValueRef(std::move(lt));
+        std::puts("\nPass by rvalue ref:");
+        callByRValueRef(std::move(lt));
 
-    std::puts("\nRvalue Temp:");
-    callByRValueRef(Lifetime(2));
+        std::puts("\nPass by rvalue temp:");
+        callByRValueRef(Lifetime{2});
 
-    std::puts("");
+        std::puts("");
+    }
+    {
+        std::puts("\nReturn value:");
+        returnValue();
+
+        std::puts("\nReturn optional:");
+        returnOptional();
+
+        std::puts("\nReturn optional value:");
+        returnOptionalValue();
+    }
 }
