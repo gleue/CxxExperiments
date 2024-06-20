e04c1925bb3ca17335f02660af551cd2f048306c
Tim Gleue <tim@gleue-interactive.com>
2024-06-20 09:07:22 +0200

    Add testcases

--- 05-SellingWood/main.cpp
+++ 05-SellingWood/main.cpp
@@ -21,4 +21,22 @@ int main() {
 
         std::cout << maxMoney << '\n';
     }
+    std::puts("Testcase 27: ");
+    {
+        std::vector<std::vector<int>> prices{{2,3,15},{5,2,10},{1,3,27},{1,2,23},{5,1,11},{4,7,23}};
+
+        Solution solution;
+        auto maxMoney = solution.sellingWood(5, 9, prices);
+
+        std::cout << maxMoney << '\n';
+    }
+    std::puts("Testcase 30: ");
+    {
+        std::vector<std::vector<int>> prices{{4,12,3},{5,3,18},{1,2,10},{2,12,22},{2,6,12},{3,8,30},{2,3,30},{3,12,17},{2,4,20},{3,3,19},{5,10,6},{4,10,26},{4,1,28},{5,9,9},{5,13,2},{2,5,19},{3,4,2}};
+
+        Solution solution;
+        auto maxMoney = solution.sellingWood(5, 13, prices);
+
+        std::cout << maxMoney << '\n';
+    }
 }
