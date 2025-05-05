24878ddcde925a5e117f1758cae4bb04a93551d2
Tim Gleue <tim@gleue-interactive.com>
2025-05-05 21:28:21 +0200

    Add command line options and stop gracefully

--- 10-CakeMachine/cake_machine.cpp
+++ 10-CakeMachine/cake_machine.cpp
@@ -1,6 +1,7 @@
 #include "Workstation.h"
 
 #include <chrono>
+#include <getopt.h>
 #include <iostream>
 #include <random>
 
@@ -17,11 +18,63 @@ std::chrono::duration<long double> prepTimerFunc() {
     return millis;
 }
 
-int reportInterval = 10;
+constexpr int defaultNumberOfCakes = 0;
+constexpr int defaultReportInterval = 10;
+
+int numberOfCakes = defaultNumberOfCakes;
+int reportInterval = defaultReportInterval;
+
+void usage(const char* argv0) {
+    std::cerr << "Usage\n\n  " << argv0 << " [options]\n\n";
+    std::cerr << "Creates an infinite number of cakes and reports progress every 10 seconds.\n\n";
+    std::cerr << "Options\n\n";
+    std::cerr << "  -h | --help                     = Print this help\n" \
+                 "  -c | --cakes number             = Number of cakes to produce (0 == infinite)\n" \
+                 "  -r | --report seconds           = Seconds between progress reports\n\n";
+    std::cerr << std::endl;
+}
+
+int parseArguments(int argc, char* argv[]) {
+
+    static const option longOptions[] = {
+        { "help", 0, nullptr, 'h'},
+        { "cakes", 1, nullptr, 'c' },
+        { "report", 1, nullptr, 'r' },
+        { nullptr, 0, nullptr, 0 },
+    };
+
+    int opt = -1;
+
+    while ((opt = getopt_long(argc, argv, "hc:r:", longOptions, nullptr)) > 0) {
+        switch (opt) {
+            case 'h':
+                usage(argv[0]);
+                exit(0);
+            case 'c':
+                numberOfCakes = atoi(optarg);
+                if (numberOfCakes < 0) numberOfCakes = defaultNumberOfCakes;
+                break;
+            case 'r':
+                reportInterval = atoi(optarg);
+                if (reportInterval < 1) reportInterval = defaultReportInterval;
+                break;
+            case '?':
+                usage(argv[0]);
+                exit(1);
+        }
+    }
+
+    return optind;
+}
 
 int main(int argc, char* argv[]) {
 
-    if (argc > 1) reportInterval = std::max(std::atoi(argv[1]), 1);
+    auto index = parseArguments(argc, argv);
+
+    if (argc - optind != 0) {
+        usage(argv[0]);
+        return 1;
+    }
 
     puts("Initializing...");
     auto shelf1{ std::make_shared<Shelf>()};
@@ -41,16 +94,16 @@ int main(int argc, char* argv[]) {
     Workstation<1, decltype(outTimer)> total("Total", shelf3, nullptr, outTimer);
 
     puts("Working...");
-    auto startTime = std::chrono::high_resolution_clock::now();
+    auto startTime = std::chrono::steady_clock::now();
     std::size_t curTotal = 0;
-    while (true) {
+    while (numberOfCakes == 0 || curTotal < numberOfCakes) {
         for (std::size_t i = 0; i < reportInterval; std::this_thread::sleep_for(1s), i++) {
             std::cout << ((i % 10) ? "." : ":") << std::flush;
         }
 
         auto newTotal = total.getCount();
         auto delta = newTotal - curTotal;
-        auto reportTime = std::chrono::high_resolution_clock::now();
+        auto reportTime = std::chrono::steady_clock::now();
         std::cout << " " << prep << " > " << bake << " > " << pack << " = Total: " << newTotal << " (+" << delta << ")";
         curTotal = newTotal;
         
@@ -59,6 +112,16 @@ int main(int argc, char* argv[]) {
         auto throughput = double(curTotal) * factor;
         std::cout << " [" << throughput << "/m]\n";
     }
+
+    puts("Finishing...");
+    shelf1->close();
+    shelf2->close();
+    shelf3->close();
+
+    prep.stop();
+    bake.stop();
+    pack.stop();
+    total.stop();
 
     return 0;
 }
